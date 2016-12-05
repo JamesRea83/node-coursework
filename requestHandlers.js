85adef21a5b7d5bec5522d4f9ec76c51532ecb9b
JamesRea83 <jamesrea83@gmail.com>
2016-12-05 18:58:23 +0000

    it works!

--- requestHandlers.js
+++ requestHandlers.js
@@ -6,6 +6,8 @@ function timeStamper(pathname, response) {
 	var testPath = url.parse(pathname, true).pathname.replace(/\//, "")
 	console.log("timeStapmer fired with: " + testPath)
 
+var monthArr = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+
 
 	var result = {
     "unix": "",
@@ -15,32 +17,33 @@ function timeStamper(pathname, response) {
 		
 		//handle natural date
     
-		var d = new Date(testPath.replace(/%20/g, " ")).toLocaleDateString().split(" ")
-			console.log(d.toString().split("/"))
+		var d = new Date(testPath.replace(/%20/g, " ")).toLocaleDateString().split("/")
+			console.log("d = " + d)
     		result.unix = Date.parse(d) / 1000;
 	
-		result.natural = d[1] + " " + d[2] + " " + d[3]; 
+		
 
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(JSON.stringify(result));
-		response.end();
+		result.natural = d[1] + "th " + monthArr[d[0] - 1] + " " + d[2];
+
+
     
 	
 } else {
 
 		//handle unix date
 
-		var d = new Date(testPath * 1000).toLocaleDateString().split(" ")
+		var d = new Date(testPath * 1000).toLocaleDateString().split("/")
    	
     		result.unix = Date.parse(d) / 1000;
    	
-		result.natural = d[1] + " " + d[2] + " " + d[3];    
+		result.natural = d[1] + "th " + monthArr[d[0] - 1] + " " + d[2];   
 
+
+	
+}	
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write(JSON.stringify(result));
 		response.end();
-	
-}	
 
 }
 
